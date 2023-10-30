4b7b70bcb81536a802b91329bade2a00a295384f
Jeffry Degrande <jeffry@jeffrydegrande.com>
2023-10-30 01:34:34 -0300

    Updates + formatting

--- .config/nvim/lua/jeffrydegrande/plugins/bufferline.lua
+++ .config/nvim/lua/jeffrydegrande/plugins/bufferline.lua
@@ -1,11 +1,12 @@
-return {
-	"akinsho/bufferline.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	version = "*",
-	opts = {
-		options = {
-			mode = "tabs",
-			separator_style = "slant",
-		},
-	},
-}
+return {}
+-- return {
+-- 	"akinsho/bufferline.nvim",
+-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+-- 	version = "*",
+-- 	opts = {
+-- 		options = {
+-- 			mode = "buffers",
+-- 			separator_style = "slant",
+-- 		},
+-- 	},
+-- }
