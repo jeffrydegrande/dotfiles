7fa67e364beae57eae2052ca7935722ae6a5e5b5
Jeffry Degrande <jeffry@jeffrydegrande.com>
2024-01-08 23:00:31 -0300

    Stealing some keymaps from primeagen

--- .config/nvim/lua/jeffrydegrande/core/keymaps.lua
+++ .config/nvim/lua/jeffrydegrande/core/keymaps.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = " "
-local keymap = vim.keymap
 
-keymap.set("n", "<leader>x", ":bn<cr>")
-keymap.set("n", "<leader>z", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bn<cr>")
+vim.keymap.set("n", "<leader>z", ":bp<cr>")
+
+-- move lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- tmux sessionizer from inside vim <3
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
